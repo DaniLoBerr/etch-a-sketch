8c3ef9072c1abf3929427c8208d76d639c38cd1a
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-08-02 18:22:02 +0200

    Change grid feature: change button functionality
    Create grid change button functionality in script.js file. If the user
    presses the button, a window will appear asking the user what number of
    squares per side wants for the new grid. Once entered, the existing
    grid will be removed, and a new grid will be generated in the same total
    space as before so the user got a new sketch pad.
    
    Remove screen/Sketch pad border

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 const gridSquaresContainer = document.querySelector("#grid-squares-container");
+let squaresPerSide = 0;
 
-// Create the initial 16x16 squares screen
-let squaresPerSide = 16;
-const totalSquares = squaresPerSide ** 2;
-for (let i = 0; i < totalSquares; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  gridSquaresContainer.appendChild(square);
+function changeGrid(newSquaresPerSide) {
+  squaresPerSide = newSquaresPerSide;
+  totalSquares = squaresPerSide ** 2;
+  // Add the squares
+  for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    gridSquaresContainer.appendChild(square);
+  }
+  // Create the hover effect
+  const gridSquares = document.querySelectorAll(".square");
+  gridSquares.forEach(
+    (square) => {
+      square.addEventListener("mouseover", () => {
+        square.classList.add("hovered-square");
+      });
+    }
+  );
 }
 
-const gridSquares = document.querySelectorAll(".square");
-gridSquares.forEach(
-  (square) => {
-    square.addEventListener("mouseover", () => {
-      square.classList.add("hovered-square");
-    });
+function resetGrid() {
+  const gridSquares = document.querySelectorAll(".square");
+  gridSquares.forEach(
+    (square) => {
+      gridSquaresContainer.removeChild(square);
+    }
+  );
+}
+
+// Create initial 16x16 sketch pad
+changeGrid(16);
+
+const changeGridButton = document.querySelector("#change-grid-button");
+changeGridButton.addEventListener("click", () => {
+  squaresPerSide = +prompt("How many squares per side do you want? (Max 100)");
+  if (squaresPerSide >= 1 && squaresPerSide <= 100) {
+    resetGrid();
+    changeGrid(squaresPerSide);
+    // Resize squares
+    const gridSquares = document.querySelectorAll(".square");
+    const gridContainerSideSizeInPixels = 600;
+    const newSquareProportion = gridContainerSideSizeInPixels / squaresPerSide;
+    gridSquares.forEach(
+      (square) => {
+        square.setAttribute("style", `flex: 1 1 ${newSquareProportion}px;`);
+      }
+    );
+  } else {
+    alert("El dato introducido no es válido");
   }
-);
+});
